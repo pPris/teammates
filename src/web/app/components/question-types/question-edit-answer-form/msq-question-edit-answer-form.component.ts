0a56f3f6460e4f21b71ff99d45cd109092cc316a
ppris <49683462+pPris@users.noreply.github.com>
2021-06-22 22:12:27 +0800

    Correct event type

--- src/web/app/components/question-types/question-edit-answer-form/msq-question-edit-answer-form.component.ts
+++ src/web/app/components/question-types/question-edit-answer-form/msq-question-edit-answer-form.component.ts
@@ -50,7 +50,7 @@ export class MsqQuestionEditAnswerFormComponent
   /**
    * Updates the answers to include/exclude the Msq option specified by the index.
    */
-  updateSelectedAnswers(index: number, $event: Event): void {
+  updateSelectedAnswers(index: number, $event: KeyboardEvent): void {
     let newAnswers: string[] = [];
 
     if (!this.isNoneOfTheAboveEnabled) {
